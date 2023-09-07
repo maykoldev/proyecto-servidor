c5167a544753e570167dc53427b3083a818bae54
Maykol <138355741+maykoldev@users.noreply.github.com>
2023-09-07 12:55:50 -0600

    aca agregue selectores

--- tareas/tareas.js
+++ tareas/tareas.js
@@ -1,6 +1,11 @@
 const user = JSON.parse(localStorage.getItem('user'));
 //console.log(user);
 
+const formulario = document.querySelector('#form-todos');
+const lista = document.querySelector('#todo-list');
+const inputF = document.querySelector('#form-input');
+const cerrarBtn = document.querySelector('#cerrar-btn');
+
 if (!user) {
     //caso de que el user no este en el LS
     window.location.href = '../home/index.html';
